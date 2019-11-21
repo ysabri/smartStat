4831f31d6d698fa9a5354577547f5ae5754c3064
Yazeed Sabri <sabri.yazeed@gmail.com>
2019-11-20 21:38:08 -0600

    First commit

---
+++ src/test/java/com/smartstat/YazeedSabriApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smartstat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YazeedSabriApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
